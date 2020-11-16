67da0ebf2ab0422ef03554c099abb42b10a9a81f
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-16 09:58:14 -0500

    intial commit

---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from 'styled-components'
+import {FaBars} from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <Nav>
+      <NavLink to="/">Gtawy</NavLink>
+      <Bars />
+    </Nav>
+  )
+}
+
+
+export default Header
+
+const Nav = styled.nav`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  z-index: 100;
+  position: relative;
+`
+
+const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+`
+
+const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./Header"
+import { GlobalStyle } from "./styles/GlobalStyles"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+        <main>{children}</main>
+    </>
+  )
+}
+
+export default Layout
