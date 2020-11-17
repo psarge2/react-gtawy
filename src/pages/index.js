100769ad10ca7edc3917579c830019910229f592
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-17 01:15:57 -0500

    added images for trips section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Hero from "../components/Hero"
+import Trips from "../components/Trips"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -8,6 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <Trips />
   </Layout>
 )
 
